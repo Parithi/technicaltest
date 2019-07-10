1b7f616587b751caab819bd9ba8a3969af384f8d
ElamParithi Arul <elamparithiarul@gmail.com>
2019-07-10 14:36:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Eyes App Mobile Technical Test Assessment
+ Eyes App - Mobile Technical Test Assessment
---
+++ src/main/kotlin/com/eyes/TechnicaltestApplication.kt
@@ -0,0 +1,11 @@
+package com.eyes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechnicaltestApplication
+
+fun main(args: Array<String>) {
+	runApplication<TechnicaltestApplication>(*args)
+}
